a42d68b0b95dbb40f890b3f31a35b0c47b3631b8
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2024-05-11 22:21:33 +0600

    Update allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+jok$)#6wf&r+!qzob!b&7om@kb2=yf=6e(_hxazhglve95!e&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
